759982abe42972d7622cdc4bf6c282af0c2655b7
Tomek <email@email.com>
2014-03-23 11:56:20 +0100

    created initial structure

---
+++ add_potential_reviewers/add_potential_reviewers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Main part of script
+"""
+
+
+def main():
+    print "Main part of script"
+
+
+if __name__ == "__main__":
+    main()
---
+++ add_potential_reviewers/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+
+setup(name='Add Potential Reviewers',
+      version='0.1',
+      author='Tomasz Kolek',
+      author_email='tomasz-kolek@o2.pl',
+      scripts=["add_potential_reviewers.py"]
+      )
