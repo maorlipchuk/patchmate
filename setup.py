5b37e933561aba588ca2080fe4261248c069f467
David Caro <dcaroest@redhat.com>
2014-06-18 17:16:22 +0200

    Added tox configuration
    * Modified setup.py bug to build properly

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(name='Patchmage',
       version='0.1',
       author='Tomasz Kolek, Maor Lipchuk',
       author_email='tomasz-kolek@o2.pl, mlipchuk@redhat.com',
-      scripts=["script/add_reviewers.py"],
+      scripts=["patchmate/script/add_reviewers.py"],
       packages=find_packages()
       )
