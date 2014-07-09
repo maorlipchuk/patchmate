463c8bd5a4870bc8bcd29b8e5eaa5e4250d51b83
Tomasz Kolek <tomasz-kolek@o2.pl>
2014-07-09 23:30:41 +0200

    changes in git adapter

--- patchmate/heuristics/blame_heuristic/blame_heuristic_settings.py
+++ patchmate/heuristics/blame_heuristic/blame_heuristic_settings.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-git_log_command = "git log {since}^..{until} --format=%h"
+git_log_command = "git log {since}..{until} --format=%h"
 get_changed_files_in_commit = 'git diff-tree --no-commit-id --name-only -r {commit_id}'
 get_concrete_file_commit_info = 'git show -U0 {commit_id} {file_path}'
 git_blame_command = "git blame {commit_id} {file_path} -L{line},{line} -e"
--- patchmate/heuristics/blame_heuristic/git_adapter.py
+++ patchmate/heuristics/blame_heuristic/git_adapter.py
@@ -9,16 +9,19 @@ from blame_heuristic_settings import get_concrete_file_commit_info, git_blame_co
 class GitAdapter(object):
     def __init__(self, repository_path):
         self.repository_path = repository_path
+        print self.repository_path
 
     def _execute_command(self, command):
-        return subprocess.check_output(command, cwd=self.repository_path)
+        return subprocess.check_output(command, cwd=self.repository_path, shell=True)
 
     def get_current_user_email(self):
         return self._execute_command(get_email_command).strip()
 
     def get_commits_from_range(self, youngest_commit, oldest_commit):
         all_commits = self._execute_command(git_log_command.format(since=oldest_commit, until=youngest_commit)).splitlines()
-        return all_commits[::-1]
+        all_commits.append(oldest_commit)
+        all_commits = all_commits[::-1]
+        return all_commits
 
     def get_changed_files_in_commit(self, commit_id):
         return self._execute_command(get_changed_files_in_commit.format(commit_id=commit_id)).splitlines()
