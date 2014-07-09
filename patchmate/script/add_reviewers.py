c73ee8f390897594b59677e931fed5637449dcee
Tomasz Kolek <tomasz-kolek@o2.pl>
2014-07-09 23:30:56 +0200

    commit script version for test

--- patchmate/script/add_reviewers.py
+++ patchmate/script/add_reviewers.py
@@ -6,17 +6,30 @@ Main part of script
 import os
 import argparse
 from patchmate.helpers.settings_updater import update_settings
-from patchmate.heuristics import BlameHeuristic
+from patchmate.heuristics import BlameHeuristic, OwnerFilesHeuristic
 
+
+def _print_results(result):
+    print "MAINTAINERS"
+    for i in result.maintainers.receivers:
+        print i
+
+    print "CC"
+    for i in result.cc.receivers:
+        print i
 
 
 def main(args):
-    update_settings(args.config)
+    #update_settings(args.config)
+    #reviewers = set([])
+    #BlameHeuristic(args.repo_path, args.youngest_commit, args.oldest_commit).get_reviewers()
+    #print "\n\n"
+    result = OwnerFilesHeuristic(args.repo_path, args.youngest_commit, args.oldest_commit).get_reviewers()
 
-    BlameHeuristic(args.repo_path, args.youngest_commit, args.oldest_commit).get_reviewers()
-    print "Get reviewers from heuristics"
-    print "Send an email"
-    print "Update reviewers"
+    _print_results(result)
+
+    #print "Send an email"
+    #print "Update reviewers"
 
 
 if __name__ == "__main__":
