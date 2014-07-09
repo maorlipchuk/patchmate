30e00ccccbb8c8bf61f8430ac006aac4253df1b1
Tomasz Kolek <tomasz-kolek@o2.pl>
2014-07-09 23:31:11 +0200

    fixed package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 
-setup(name='Patchmage',
+setup(name='Patchmate',
       version='0.1',
       author='Tomasz Kolek, Maor Lipchuk',
       author_email='tomasz-kolek@o2.pl, mlipchuk@redhat.com',
