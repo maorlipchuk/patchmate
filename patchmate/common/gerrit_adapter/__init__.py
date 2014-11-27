524b7f25c5bad713f4e0df84b1aa8e3766fdd2e2
Tomasz Kolek <tomasz-kolek@o2.pl>
2014-11-27 17:50:52 +0100

    changed add reviewers by email to name

--- patchmate/common/gerrit_adapter/__init__.py
+++ patchmate/common/gerrit_adapter/__init__.py
@@ -6,4 +6,4 @@ from gerrit_adapter import GerritAdapter
 def add_reviewers_to_gerrit(username, password, url, change_id, reviewers):
     gerrit_adapter_object = GerritAdapter(url, username, password)
     for reviewer in reviewers:
-        gerrit_adapter_object.add_reviewer(change_id, reviewer.email)
+        gerrit_adapter_object.add_reviewer(change_id, reviewer.name)
