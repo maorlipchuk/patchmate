a10a8b64eb17ef87451fa97778b17d6244d586af
Tomasz Kolek <tomasz-kolek@o2.pl>
2015-01-25 17:47:13 +0100

    added file handler to logger

--- patchmate/common/logger/logger.py
+++ patchmate/common/logger/logger.py
@@ -4,8 +4,15 @@ import logging
 
 
 logger = logging.getLogger("PatchMate Logger")
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
 stream_handler = logging.StreamHandler()
 stream_handler.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 stream_handler.setFormatter(formatter)
+
+file_handler = logging.FileHandler("patchmate_logs.log")
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+
 logger.addHandler(stream_handler)
+logger.addHandler(file_handler)
